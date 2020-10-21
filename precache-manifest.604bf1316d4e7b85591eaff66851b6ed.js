d551e78448b4a67e141d45e1364f495d759f77b3
Giang Le <gianglv@vnext.com.vn>
2020-10-21 14:37:19 +0700

    Updates

---
+++ precache-manifest.604bf1316d4e7b85591eaff66851b6ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d627b21206159f92568371402da45dbe",
+    "url": "/excel-validator/index.html"
+  },
+  {
+    "revision": "ed0e231f0af527189a28",
+    "url": "/excel-validator/static/css/2.306baa07.chunk.css"
+  },
+  {
+    "revision": "7c72c51a716bb0c38b89",
+    "url": "/excel-validator/static/css/main.9a88ee37.chunk.css"
+  },
+  {
+    "revision": "ed0e231f0af527189a28",
+    "url": "/excel-validator/static/js/2.2acbfa4a.chunk.js"
+  },
+  {
+    "revision": "88994134440339a25a710f1d3c2091f4",
+    "url": "/excel-validator/static/js/2.2acbfa4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c72c51a716bb0c38b89",
+    "url": "/excel-validator/static/js/main.06ea50d8.chunk.js"
+  },
+  {
+    "revision": "dede9448cb088835c8ed",
+    "url": "/excel-validator/static/js/runtime-main.4adaba59.js"
+  },
+  {
+    "revision": "2b9b4872cd25494093c1eb14f0264a0b",
+    "url": "/excel-validator/static/media/jsoneditor-icons.2b9b4872.svg"
+  }
+]);
